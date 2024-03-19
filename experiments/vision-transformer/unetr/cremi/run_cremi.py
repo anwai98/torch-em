6b914c515165bdd9577cd1488831e00dbe620152
anwai98 <anwai.archit@gmail.com>
2024-03-20 00:53:42 +0100

    Update UNETR experiments for CREMI

--- experiments/vision-transformer/unetr/cremi/run_cremi.py
+++ experiments/vision-transformer/unetr/cremi/run_cremi.py
@@ -1,23 +1,13 @@
 import os
 import argparse
 import numpy as np
-import pandas as pd
-from glob import glob
-from tqdm import tqdm
-
-import imageio.v3 as imageio
 
 import torch
-
 import torch_em
-from torch_em.util import segmentation
-from torch_em.data import MinInstanceSampler
-from torch_em.model import get_vimunet_model
-from torch_em.transform.raw import standardize
+from torch_em.model import UNETR
 from torch_em.data.datasets import get_cremi_loader
+from torch_em.data import MinInstanceSampler
 from torch_em.loss import DiceLoss, LossWrapper, ApplyAndRemoveMask, DiceBasedDistanceLoss
-
-from elf.evaluation import mean_segmentation_accuracy
 
 
 ROOT = "/scratch/usr/nimanwai"
@@ -28,6 +18,13 @@ OFFSETS = [
     [-9, 0], [0, -9],
     [-27, 0], [0, -27]
 ]
+
+MODELS = {
+    "vit_t": "/scratch/usr/nimanwai/models/segment-anything/checkpoints/vit_t_mobile_sam.pth",
+    "vit_b": "/scratch/usr/nimanwai/models/segment-anything/checkpoints/sam_vit_b_01ec64.pth",
+    "vit_l": "/scratch/usr/nimanwai/models/segment-anything/checkpoints/sam_vit_l_0b3195.pth",
+    "vit_h": "/scratch/usr/nimanwai/models/segment-anything/checkpoints/sam_vit_h_4b8939.pth"
+}
 
 
 def get_loaders(args, patch_shape=(1, 512, 512)):
@@ -121,36 +118,33 @@ def get_save_root(args):
     return save_root
 
 
-def run_cremi_training(args):
+def run_cremi_unetr_training(args, device):
     # the dataloaders for cremi dataset
     train_loader, val_loader = get_loaders(args)
 
-    if args.pretrained:
-        checkpoint = "/scratch/usr/nimanwai/models/Vim-tiny/vim_tiny_73p1.pth"
-    else:
-        checkpoint = None
-
     output_channels = get_output_channels(args)
 
-    # the vision-mamba + decoder (UNet-based) model
-    model = get_vimunet_model(
+    # the UNETR model
+    model = UNETR(
+        encoder=args.model_type,
         out_channels=output_channels,
-        model_type=args.model_type,
-        checkpoint=checkpoint,
-        with_cls_token=True
+        use_sam_stats=args.pretrained,
+        encoder_checkpoint=MODELS[args.model_type] if args.pretrained else None,
+        final_activation="Sigmoid"
     )
+    model.to(device)
 
     save_root = get_save_root(args)
 
     # loss function
     loss = get_loss_function(args)
 
-    # trainer for the segmentation task
     trainer = torch_em.default_segmentation_trainer(
-        name="cremi-vimunet",
+        name="cremi-unetr",
         model=model,
         train_loader=train_loader,
         val_loader=val_loader,
+        device=device,
         learning_rate=1e-4,
         loss=loss,
         metric=loss,
@@ -158,91 +152,25 @@ def run_cremi_training(args):
         save_root=save_root,
         compile_model=False
     )
-    trainer.fit(iterations=args.iterations)
+
+    trainer.fit(args.iterations)
 
 
-def run_cremi_inference(args, device):
-    output_channels = get_output_channels(args)
-
-    save_root = get_save_root(args)
-
-    checkpoint = os.path.join(save_root, "checkpoints", "cremi-vimunet", "best.pt")
-
-    # the vision-mamba + decoder (UNet-based) model
-    model = get_vimunet_model(
-        out_channels=output_channels,
-        model_type=args.model_type,
-        with_cls_token=True,
-        checkpoint=checkpoint
-    )
-
+def run_cremi_unetr_inference(args):
     raise NotImplementedError
-
-    test_image_dir = os.path.join(ROOT, "data", "livecell", "images", "livecell_test_images")
-    all_test_labels = glob(os.path.join(ROOT, "data", "livecell", "annotations", "livecell_test_images", "*", "*"))
-
-    res_path = os.path.join(save_root, "results.csv")
-    if os.path.exists(res_path):
-        print(pd.read_csv(res_path))
-        print(f"The result is saved at {res_path}")
-        return
-
-    msa_list, sa50_list = [], []
-
-    for label_path in tqdm(all_test_labels):
-        labels = imageio.imread(label_path)
-        image_id = os.path.split(label_path)[-1]
-
-        image = imageio.imread(os.path.join(test_image_dir, image_id))
-        image = standardize(image)
-
-        tensor_image = torch.from_numpy(image)[None, None].to(device)
-
-        predictions = model(tensor_image)
-        predictions = predictions.squeeze().detach().cpu().numpy()
-
-        if args.boundaries:
-            fg, bd = predictions
-            instances = segmentation.watershed_from_components(bd, fg)
-
-        elif args.affinities:
-            fg, affs = predictions[0], predictions[1:]
-            instances = segmentation.mutex_watershed_segmentation(fg, affs, offsets=OFFSETS)
-
-        elif args.distances:
-            fg, cdist, bdist = predictions
-            instances = segmentation.watershed_from_center_and_boundary_distances(
-                cdist, bdist, fg, min_size=50,
-                center_distance_threshold=0.5,
-                boundary_distance_threshold=0.6,
-                distance_smoothing=1.0
-            )
-
-        msa, sa_acc = mean_segmentation_accuracy(instances, labels, return_accuracies=True)
-        msa_list.append(msa)
-        sa50_list.append(sa_acc[0])
-
-    res = {
-        "CREMI": "Metrics",
-        "mSA": np.mean(msa_list),
-        "SA50": np.mean(sa50_list)
-    }
-    df = pd.DataFrame.from_dict([res])
-    df.to_csv(res_path)
-    print(df)
-    print(f"The result is saved at {res_path}")
 
 
 def main(args):
     assert (args.boundaries + args.affinities + args.distances) == 1
 
     print(torch.cuda.get_device_name() if torch.cuda.is_available() else "GPU not available, hence running on CPU")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     if args.train:
-        run_cremi_training(args)
+        run_cremi_unetr_training(args, device)
 
     if args.predict:
-        run_cremi_inference(args)
+        run_cremi_unetr_inference(args)
 
 
 if __name__ == "__main__":
@@ -250,7 +178,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input", type=str, default=os.path.join(ROOT, "data", "cremi"))
     parser.add_argument("--iterations", type=int, default=int(1e5))
     parser.add_argument("-s", "--save_root", type=str, default=os.path.join(ROOT, "experiments", "vimunet"))
-    parser.add_argument("-m", "--model_type", type=str, default="vim_t")
+    parser.add_argument("-m", "--model_type", type=str, default="vit_b")
 
     parser.add_argument("--pretrained", action="store_true")
 
