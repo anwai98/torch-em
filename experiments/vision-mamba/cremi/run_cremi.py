7a8d443597fcfe5034054203dd75fa3229342766
anwai98 <anwai.archit@gmail.com>
2024-03-30 15:32:50 +0100

    Update inference

--- experiments/vision-mamba/cremi/run_cremi.py
+++ experiments/vision-mamba/cremi/run_cremi.py
@@ -13,14 +13,19 @@ import torch_em
 from torch_em.util import segmentation
 from torch_em.data import MinInstanceSampler
 from torch_em.model import get_vimunet_model
-from torch_em.transform.raw import standardize
 from torch_em.data.datasets import get_cremi_loader
+from torch_em.util.prediction import predict_with_halo
 from torch_em.loss import DiceLoss, LossWrapper, ApplyAndRemoveMask, DiceBasedDistanceLoss
 
+
+import elf.segmentation.multicut as mc
+import elf.segmentation.features as feats
+import elf.segmentation.watershed as ws
 from elf.evaluation import mean_segmentation_accuracy
 
 
 ROOT = "/scratch/usr/nimanwai"
+CREMI_TEST_ROOT = "/scratch/projects/nim00007/sam/data/cremi/slices_original"
 
 OFFSETS = [
     [-1, 0], [0, -1],
@@ -161,6 +166,71 @@ def run_cremi_training(args):
     trainer.fit(iterations=args.iterations)
 
 
+def _do_bd_multicut_watershed(bd):
+    ws_seg, max_id = ws.distance_transform_watershed(bd, threshold=0.5, sigma_seeds=2.0)
+
+    # compute the region adjacency graph
+    rag = feats.compute_rag(ws_seg)
+
+    # compute the edge costs
+    costs = feats.compute_boundary_features(rag, bd)[:, 0]
+
+    # transform the edge costs from [0, 1] to  [-inf, inf], which is
+    # necessary for the multicut. This is done by intepreting the values
+    # as probabilities for an edge being 'true' and then taking the negative log-likelihood.
+
+    # in addition, we weight the costs by the size of the corresponding edge
+    # for z and xy edges
+    z_edges = feats.compute_z_edge_mask(rag, ws_seg)
+    xy_edges = np.logical_not(z_edges)
+    edge_populations = [z_edges, xy_edges]
+    edge_sizes = feats.compute_boundary_mean_and_length(rag, bd)[:, 1]
+    costs = mc.transform_probabilities_to_costs(costs, edge_sizes=edge_sizes, edge_populations=edge_populations)
+
+    # run the multicut partitioning, here, we use the kernighan lin
+    # heuristics to solve the problem, introduced in
+    # http://xilinx.asia/_hdl/4/eda.ee.ucla.edu/EE201A-04Spring/kl.pdf
+    node_labels = mc.multicut_kernighan_lin(rag, costs)
+
+    # map the results back to pixels to obtain the final segmentation
+    seg = feats.project_node_labels_to_pixels(rag, node_labels)
+
+    return seg
+
+
+def _do_affs_multicut_watershed(affs, offsets):
+    # first, we have to make a single channel input map for the watershed,
+    # which we obtain by averaging the affinities
+    boundary_input = np.mean(affs, axis=0)
+
+    ws_seg, max_id = ws.distance_transform_watershed(boundary_input, threshold=0.25, sigma_seeds=2.0)
+
+    # compute the region adjacency graph
+    rag = feats.compute_rag(ws_seg)
+
+    # compute the edge costs
+    # the offsets encode the pixel transition encoded by the
+    # individual affinity channels. Here, we only have nearest neighbor transitions
+    costs = feats.compute_affinity_features(rag, affs, offsets)[:, 0]
+
+    # transform the edge costs from [0, 1] to  [-inf, inf], which is
+    # necessary for the multicut. This is done by intepreting the values
+    # as probabilities for an edge being 'true' and then taking the negative log-likelihood.
+    # in addition, we weight the costs by the size of the corresponding edge
+    edge_sizes = feats.compute_boundary_mean_and_length(rag, boundary_input)[:, 1]
+    costs = mc.transform_probabilities_to_costs(costs, edge_sizes=edge_sizes)
+
+    # run the multicut partitioning, here, we use the kernighan lin
+    # heuristics to solve the problem, introduced in
+    # http://xilinx.asia/_hdl/4/eda.ee.ucla.edu/EE201A-04Spring/kl.pdf
+    node_labels = mc.multicut_kernighan_lin(rag, costs)
+
+    # map the results back to pixels to obtain the final segmentation
+    seg = feats.project_node_labels_to_pixels(rag, node_labels)
+
+    return seg
+
+
 def run_cremi_inference(args, device):
     output_channels = get_output_channels(args)
 
@@ -176,43 +246,40 @@ def run_cremi_inference(args, device):
         checkpoint=checkpoint
     )
 
-    raise NotImplementedError
-
-    test_image_dir = os.path.join(ROOT, "data", "livecell", "images", "livecell_test_images")
-    all_test_labels = glob(os.path.join(ROOT, "data", "livecell", "annotations", "livecell_test_images", "*", "*"))
+    all_test_images = glob(os.path.join(CREMI_TEST_ROOT, "raw", "cremi_test_*.tif"))
+    all_test_labels = glob(os.path.join(CREMI_TEST_ROOT, "labels", "cremi_test_*.tif"))
 
     res_path = os.path.join(save_root, "results.csv")
-    if os.path.exists(res_path):
+    if os.path.exists(res_path) and not args.force:
         print(pd.read_csv(res_path))
         print(f"The result is saved at {res_path}")
         return
 
     msa_list, sa50_list = [], []
-
-    for label_path in tqdm(all_test_labels):
+    for image_path, label_path in tqdm(zip(all_test_images, all_test_labels), total=len(all_test_images)):
+        image = imageio.imread(image_path)
         labels = imageio.imread(label_path)
-        image_id = os.path.split(label_path)[-1]
 
-        image = imageio.imread(os.path.join(test_image_dir, image_id))
-        image = standardize(image)
-
-        tensor_image = torch.from_numpy(image)[None, None].to(device)
-
-        predictions = model(tensor_image)
-        predictions = predictions.squeeze().detach().cpu().numpy()
+        predictions = predict_with_halo(
+            image, model, [device], block_shape=[512, 512], halo=[128, 128], disable_tqdm=True,
+        )
 
         if args.boundaries:
-            fg, bd = predictions
-            instances = segmentation.watershed_from_components(bd, fg)
+            bd = predictions.squeeze()
+
+            # instances = segmentation.watershed_from_components(bd, np.ones_like(bd))
+            instances = _do_bd_multicut_watershed(bd)
 
         elif args.affinities:
-            fg, affs = predictions[0], predictions[1:]
-            instances = segmentation.mutex_watershed_segmentation(fg, affs, offsets=OFFSETS)
+            affs = predictions
+
+            # instances = segmentation.mutex_watershed_segmentation(np.ones_like(labels), affs, offsets=OFFSETS)
+            instances = _do_affs_multicut_watershed(affs[:2], OFFSETS[:2])
 
         elif args.distances:
             fg, cdist, bdist = predictions
             instances = segmentation.watershed_from_center_and_boundary_distances(
-                cdist, bdist, fg, min_size=50,
+                cdist, bdist, np.ones_like(fg), min_size=50,
                 center_distance_threshold=0.5,
                 boundary_distance_threshold=0.6,
                 distance_smoothing=1.0
@@ -223,7 +290,7 @@ def run_cremi_inference(args, device):
         sa50_list.append(sa_acc[0])
 
     res = {
-        "CREMI": "Metrics",
+        "LiveCELL": "Metrics",
         "mSA": np.mean(msa_list),
         "SA50": np.mean(sa50_list)
     }
@@ -238,11 +305,13 @@ def main(args):
 
     print(torch.cuda.get_device_name() if torch.cuda.is_available() else "GPU not available, hence running on CPU")
 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     if args.train:
         run_cremi_training(args)
 
     if args.predict:
-        run_cremi_inference(args)
+        run_cremi_inference(args, device)
 
 
 if __name__ == "__main__":
@@ -256,6 +325,8 @@ if __name__ == "__main__":
 
     parser.add_argument("--train", action="store_true")
     parser.add_argument("--predict", action="store_true")
+
+    parser.add_argument("--force", action="store_true")
 
     parser.add_argument("--boundaries", action="store_true")
     parser.add_argument("--affinities", action="store_true")
